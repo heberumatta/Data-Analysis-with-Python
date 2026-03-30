e8628ca7b2acd87e1619e2aca930254a23ac6db8
heberumatta <matteheber8@gmail.com>
2026-03-30 10:15:32 -0300

    Enhance main.py with additional data processing steps and comments for handling missing values and data binning

--- Module 2/main.py
+++ Module 2/main.py
@@ -16,9 +16,12 @@ import numpy as np
 path = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(path, header=None)
 
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style","drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type","num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower","peak-rpm","city-mpg","highway-mpg","price"]
+df.columns = headers
+
 #If you want to see a specific column, you can use the following code:
 #df['column_name'] #this will show the specified column of the data frame
-df[0] #this will show the first column of the data frame
+#df["price"] #this will show the first column of the data frame
 
 #Dealing with missing values
 #Missing values occur when no data value is stored for the variable in an observation.
@@ -39,6 +42,10 @@ df[0] #this will show the first column of the data frame
 #   To replace if we want to calculate the mean, median or mode of a column, we can use the following code:
 #   mean = df[column_name].mean()
 
+# df.dropna(subset=["price"], axis=0, inplace=True)
+# mean_price = df["price"].mean()
+# df.replace('?', mean_price, inplace=True)
+
 #Data formatting
 #Data are usually collected from different places and stored in different formats.
 #Bringing data into a common standard of expression allows users to make meaningful comparison.
@@ -50,4 +57,12 @@ df[0] #this will show the first column of the data frame
 #df.rename(columns={mpg_column: "L/100km"}, inplace=True)
 
 #When we apliy calculations there can be incorrect data types, we can use the following code to change the data type of a column:
-#df[column_name] = df[column_name].astype(new_data_type)
+# df["price"] = df["price"].astype("float")
+
+#Binning: grouping values into bins, convert numeric into categorical variables, group a set numerical values into a set of "bins".
+#First, we use the numpy function linspace() to return the array bins.
+# bins = np.linspace(min(df["price"]), max(df["price"]), 4)
+# group_names = ["Low", "Medium", "High"]
+# df["price_binned"] = pd.cut(df["price"], bins, labels=group_names, include_lowest=True)
+
+#Turning categorical values to numeric variables
