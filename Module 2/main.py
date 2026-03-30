1188ad4e5517002f89313bd779c8209378d8e88c
heberumatta <matteheber8@gmail.com>
2026-03-29 21:09:59 -0300

    Refactor main.py to include data import and preprocessing steps

--- Module 2/main.py
+++ Module 2/main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+#Learning Objectives
+#   Identify and handle missing values
+#   Data formatting
+#   Data normalization (centering and scaling)
+#   Data Binning
+#   Turning categorical values to numeric variables
+
+#Importing libraries
+import pandas as pd
+import numpy as np
+
+#Importing the dataset
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+df = pd.read_csv(path, header=None)
+
+#If you want to see a specific column, you can use the following code:
+#df['column_name'] #this will show the specified column of the data frame
+print(df[0]) #this will show the first column of the data frame
+
+#Data Cleaning | Data preprocessing | Data Wrangling
+#The process of converting or mapping data from the initial "raw" dorm into another format,
+#in order to prepare the data for further analysis.
